63f7c8d2b9b9f38a60ff3a941abbfa2d45a271a3
dkvz <github@dkvz.eu>
2025-06-05 12:15:43 +0200

    can find snippets and placeholders

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use crate::snippets;
+use crate::snippets::{self, snippet_placeholder};
 use anyhow::{Result, anyhow};
 use clap::Parser;
 use std::io;
@@ -19,16 +19,34 @@ pub fn run() -> Result<()> {
     let args = Args::parse();
     println!("{:?}", args);
 
-    // Check if the list is requested
+    // TODO: Check if the list is requested
 
     // We should have a snippet name here or it's an error
     if args.snippet.is_none() {
         return Err(anyhow!("missing the snippet name"));
     }
+    let name = args.snippet.unwrap();
 
     // If requesting placeholders, we don't care about the input.
+    if args.placeholders {
+        let p = snippet_placeholder(&name)?;
+        println!("{}", p);
+        return Ok(());
+    }
 
-    // Processing the input:
+    // Requested an acutal snippet, processing the input:
+    let lines = input_lines();
+    let value = if lines.is_empty() {
+        snippets::snippet_value(&name, None)?
+    } else {
+        snippets::snippet_value(&name, Some(&lines))?
+    };
+
+    println!("{}", value);
+    Ok(())
+}
+
+fn input_lines() -> Vec<String> {
     let mut input_lines: Vec<String> = Vec::new();
     let lines = io::stdin().lines();
     for line in lines {
@@ -40,7 +58,5 @@ pub fn run() -> Result<()> {
         }
     }
 
-    println!("OK so we got {} elements", input_lines.len());
-
-    Ok(())
+    input_lines
 }
--- src/snippets.rs
+++ src/snippets.rs
@@ -7,18 +7,54 @@
 * are easier to test.
 */
 
-pub fn delete_me_later() -> String {
-    snippet_b_image(true, &None)
+use anyhow::{Result, anyhow};
+
+#[derive(Debug)]
+pub struct Snippet {
+    pub name: &'static str,
+    pub placeholders: Option<&'static str>,
+    pub process_snippet: fn(values: Option<&Vec<String>>) -> String,
 }
 
-fn snippet_b_image(get_placeholders: bool, values: &Option<Vec<String>>) -> String {
-    let template = "max-width (px)\n\
+// Create a big fat array
+// I think it's just faster to browse than a HashMap for what I'm doing
+// I have to update the count manually though. Rust is fun.
+const SNIPPETS: [Snippet; 1] = [Snippet {
+    name: "b-image",
+    placeholders: Some(
+        "max-width (px)\n\
         image_link image_src\n\
         alt\n\
-        legend";
-    if get_placeholders {
-        String::from(template)
-    } else {
-        String::from("IMAGE")
+        legend",
+    ),
+    process_snippet: |values| String::from("IMAGE"),
+}];
+
+fn get_snippet(name: &str) -> Result<&Snippet> {
+    let snip = SNIPPETS
+        .iter()
+        .find(|s| s.name == name && s.placeholders.is_some());
+
+    if snip.is_none() {
+        // Snippet doesn't exist.
+        return Err(anyhow!("snippet not found"));
     }
+
+    Ok(snip.unwrap())
+}
+
+// If the snippet has no placeholder, return the processed value immediately.
+pub fn snippet_placeholder(name: &str) -> Result<String> {
+    let s = get_snippet(name)?;
+
+    match s.placeholders {
+        Some(s) => Ok(String::from(s)),
+        None => Ok((s.process_snippet)(None)),
+    }
+}
+
+pub fn snippet_value(name: &str, values: Option<&Vec<String>>) -> Result<String> {
+    let s = get_snippet(name)?;
+
+    Ok((s.process_snippet)(values))
 }
